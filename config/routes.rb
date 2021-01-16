8254418ada71b89e75dbeac155495191366d49c0
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-01-16 15:15:58 +0100

    finish :)

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # Lire toutes les taches
-  get 'tasks' => 'tasks#index'
-  # Lire une taches
-  get 'task/:id' => 'tasks#show'
-  # Creer un restaurant
-  get 'tasks/new' => 'tasks#new'
-  post 'tasks' => 'tasks#create'
-  # Update un restaurant
-  get 'tasks/:id/edit' => 'tasks#edit'
-  patch 'tasks/:id' => 'tasks#update'
-  # Delete un restaurant
-  delete 'tasks/:id' => 'tasks#destroy'
+  # # Lire toutes les taches
+  # get 'tasks' => 'tasks#index'
+  # # Lire une taches
+  # get 'tasks/:id' => 'tasks#show', as: :task
+  # # Creer un restaurant
+  # get 'tasks/new' => 'tasks#new', as: :new_task
+  # post 'tasks' => 'tasks#create'
+  # # Update un restaurant
+  # get 'tasks/:id/edit' => 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id' => 'tasks#update'
+  # # Delete un restaurant
+  # delete 'tasks/:id' => 'tasks#destroy'
+
+  resources :tasks
 end

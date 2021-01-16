bdfc795d073e474c062d71aa9ce12d3cf20af22a
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-01-16 11:14:21 +0100

    created controller / routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Lire toutes les taches
+  get 'tasks' => 'tasks#index'
+  # Lire une taches
+  get 'task/:id' => 'tasks#show'
+  # Creer un restaurant
+  get 'tasks/new' => 'tasks#new'
+  post 'tasks' => 'tasks#create'
+  # Update un restaurant
+  get 'tasks/:id/edit' => 'tasks#edit'
+  patch 'tasks/:id' => 'tasks#update'
+  # Delete un restaurant
+  delete 'tasks/:id' => 'tasks#destroy'
 end
